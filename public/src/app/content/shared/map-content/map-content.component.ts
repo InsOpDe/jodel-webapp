3be051d1e6c6455be607c8c6143c3a51d9241e3b
Marcel Michelfelder <marcel.michelfelder@uni-ulm.de>
2018-04-05 20:50:15 +0200

    Added map

--- public/src/app/content/shared/map-content/map-content.component.ts
+++ public/src/app/content/shared/map-content/map-content.component.ts
@@ -1,19 +1,31 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ContentService} from "../../../content.service";
+import {CITIES} from "../../../global/cities";
+import {MAP} from "./map";
 import {MapModel} from "./map.model";
 
 
 @Component({
   selector: 'app-map-content',
-  templateUrl: './map-content.component.html'
+  templateUrl: './map-content.component.html',
+  // styleUrls: ['./map-content.component.css']
 })
 export class MapContentComponent implements OnInit {
 
   @Input() map: MapModel;
+  mapArr: object = MAP;
+  citiesCoords : object = {};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    for(let i in CITIES) {
+      let city = CITIES[i];
+      let x = city.coordinates.x;
+      let y = city.coordinates.y;
+      this.citiesCoords[y + "-" + x] = city;
+    }
   }
 
 }
