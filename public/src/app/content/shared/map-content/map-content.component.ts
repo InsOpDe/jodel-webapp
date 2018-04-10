dd9f8e9bfa9ad5ffb2f00b903137a7ab15d7f635
Marcel Michelfelder <marcel.michelfelder@uni-ulm.de>
2018-04-10 16:19:48 +0200

    Fixed mapping of city-ids

--- public/src/app/content/shared/map-content/map-content.component.ts
+++ public/src/app/content/shared/map-content/map-content.component.ts
@@ -43,31 +43,46 @@ export class MapContentComponent implements OnInit {
 
   ngOnInit() {
 
+    // map cities
     let mapCitiesDummyObj = {};
     for(let i in MapCitiesDummy){
-      mapCitiesDummyObj[MapCitiesDummy[i].loc_name] = MapCitiesDummy[i];
+      mapCitiesDummyObj[MapCitiesDummy[i].id_cities] = MapCitiesDummy[i];
     }
-
 
     for(let i in CITIES) {
       let city = CITIES[i];
       let x = city.coordinates.x;
       let y = city.coordinates.y;
+      let id = i;
 
-      if(MapCitiesDummy[i]) {
-        city.votes = MapCitiesDummy[i].votes;
+      if(MapCitiesDummy[id]) {
+        city.votes = MapCitiesDummy[id].votes;
         this.maxvote = Math.max(this.maxvote, city.votes)
+      } else {
+        city.votes = 0;
       }
-      this.citiesCoords[y + "-" + x] = city;
+
+      let cityAlreadyThere = this.citiesCoords[y + "-" + x];
+      if(cityAlreadyThere) {
+        if(cityAlreadyThere.votes < this.citiesCoords[y + "-" + x].votes) {
+          this.citiesCoords[y + "-" + x] = city;
+          this.citiesCoords[y + "-" + x].votes += cityAlreadyThere.votes;
+        } else {
+          this.citiesCoords[y + "-" + x].votes += city.votes;
+        }
+      } else {
+        this.citiesCoords[y + "-" + x] = city;
+        // console.log(this.citiesCoords[y + "-" + x].city);
+      }
     }
 
+    // calculate steps for heatmap
     let maxLogValue = Math.log(this.maxvote - 10);
     let step = maxLogValue / this.maxsteps;
 
     for(let i = 0; i <= this.maxsteps; i++) {
       this.voteindex[i] = Math.pow(Math.E, step * i)
     }
-    console.log(this.voteindex)
 
   }
 
