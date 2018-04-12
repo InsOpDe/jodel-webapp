1957d0491c0894392c5e8e35005b4a04bc0b3fb2
Tim Mend <tmend@gmx.net>
2018-04-12 18:28:20 +0200

    bla bla - commit message

--- src/api.ts
+++ src/api.ts
@@ -40,7 +40,7 @@ export class Api{
         router.use('/user', this.foo);
         router.use('/random', this.getRandomPost);
         router.use('/dummy', this.returnDummy);
-        let tmp = new JResult("Mein Name ist Tim #jo #swag", this.db);
+        let tmp = new JResult("Wasser löst irgendwie alle Probleme. Du willst abnehmen? Trink Wasser. Du hast unreine Haut? Trink Wasser. Dein Ex nervt? Ertränk ihn im Wasser. #darferdas", this.db);
         await tmp.getResult();
         console.log(JSON.stringify(tmp.toJSON()));
         //console.log(JSON.stringify(tmp.toJSON()))
